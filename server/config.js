527d7768cc41f2a7ed5b19d2130da220be1a90e6
Liuxl945 <290698200@qq.com>
2020-08-15 20:52:11 +0800

    vip.jfyf

--- server/config.js
+++ server/config.js
@@ -5,4 +5,6 @@ export const NGINX_ROOT = "C:\\Users\\Administrator\\Desktop\\nginx-1.16.0\\html
 export const NGINX_ROOT_URL = "http://y.yingliyingli.com"   //服务器
 export const API_URL = "http://localhost:10088"   //测试环境
 
-export const CORS_URL = ["https://www.yingliyingli.com","http://www.yingliyingli.com", "https://yingliyingli.com","http://yingliyingli.com", "https://admin.yingliyingli.com","http://admin.yingliyingli.com", "http://localhost:9528", "http://localhost:3000","http://localhost:4000","119.39.102.188","http://localhost:8080","http://localhost:8081","http://localhost"]
+export const CORS_URL = ["https://www.yingliyingli.com","http://www.yingliyingli.com", "https://yingliyingli.com","http://yingliyingli.com", 
+"https://admin.yingliyingli.com","http://admin.yingliyingli.com", "http://localhost:9528", "http://localhost:3000","http://localhost:4000",
+"119.39.102.188","http://localhost:8080","http://localhost:8081","http://localhost","http://serve.jfyf.com","http://vip.jfyf.com","http://h5.yingliyingli.com"]
